8b6a8fa9d01df1775b2318c2b7d0bd6204bdeb2c
Joffrey <joffreyViolette@gmail.com>
2023-09-21 17:03:08 +0200

    Quete 6 (Bonus)

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import "./App.css";
 
 const pokemonList = [
   {
-    name: "bulbasaur",
+    name: "bulbasaur", 
     imgSrc:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },

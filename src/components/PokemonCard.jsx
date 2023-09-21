79851df878133fc25f171dcb3cd085049a59a3c0
Joffrey <joffreyViolette@gmail.com>
2023-09-21 12:10:51 +0200

    Quete 3 (PokemonList+UseValue+ConditionalDisplay)

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,20 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+const pokemon = pokemonList[1];
+
 function PokemonCard() {
   return (
     <figure className="card">
-      <img
-        className="img"
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbasaur"
-      />
-      <figcaption className="name">bulbasaur</figcaption>
+      {pokemon.imgSrc ? <img className="img" src={pokemon.imgSrc} alt={pokemon.name} /> : <p>???</p>}
+      <figcaption className="name">{pokemon.name}</figcaption>
     </figure>
   );
 }
